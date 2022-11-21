6aa35c93d9cf07cb9d425492d1e92035a090a265
shdh <shdh@one.com>
2022-11-21 20:34:31 +0400

    case-5-typescript, initial commit

--- src/add.ts
+++ src/add.ts
@@ -1,6 +1,6 @@
 import * as addHelper from "./addHelper";
 
-export const add = (a, b) => {
+export const add = (a: number, b: number): string => {
     const result = addHelper.addFractions();
 
     return "I am add, " + result;
