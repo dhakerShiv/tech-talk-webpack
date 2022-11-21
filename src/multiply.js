3f4cf979eab543d2f98d32456d221c644185ba91
shdh <shdh@one.com>
2022-11-21 20:57:22 +0400

    case-6-classes, initial commit

---
+++ src/multiply.js
@@ -0,0 +1,14 @@
+export class Multiply {
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    multiplyBigNumbers() {
+        return "I am multiplyBigNumbers";
+    }
+
+    multiplyFractions() {
+        return "I am multiplyFractions";
+    }
+}
